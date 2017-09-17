ea4df504485df5faeee21a51aee3e3b33c9dbbf8
Calum Patrick <hello@calum.co>
2017-09-17 03:23:17 -0400

    profile shows logged in user's tracks, name. Home page shows logged in user's tracks, hides struff if not logged in.

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var vcs = require('../lib/version_control')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: res.locals.user });
-  console.log("USER+ "+res.locals.user);
+   if (res.locals.user) {
+  var theUser = vcs.getJSON(res.locals.user.username);
+  theUser.repositories = [{"title":"TheTitle","path":"/uploads/bryson1.mp3"}]
+} else {
+  theUser = {}
+}
+  res.render('index', {
+    title: 'Express',
+    user: res.locals.user || "",
+    profile: theUser,
+   });
+
 });
 
 module.exports = router;
