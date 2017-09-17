5136cd880085d4320433c05a6695ae613bf84969
Aamir Tahir <saamirt@gmail.com>
2017-09-17 02:52:52 -0400

    Modified merging script

--- lib/gitMusic.py
+++ lib/gitMusic.py
@@ -1,14 +1,23 @@
 from pydub import AudioSegment
 import os, sys
 
-trackA = sys.argv[2]
-trackB = sys.argv[3]
-print(trackA)
-export_path = sys.argv[1]
+path = os.getcwd()+ "\\"
+export_path = path + sys.argv[1]
+if (sys.argv[1][-1:] != '\\' and sys.argv[1][-1:] != '//'):
+    export_path += "\\"
+base_track_file = sys.argv[2]
+overlay_track_file = sys.argv[3]
+pos = sys.argv[4]
 
-trackA = AudioSegment.from_file(trackA,format="mp3")
-trackB = AudioSegment.from_file(trackB,format="mp3")
-merged_track = trackA.overlay(trackB,position=1000)
-file_handle = merged_track.export(export_path + "/merged_track.mp3",format="mp3")
+base_track = AudioSegment.from_file(path + base_track_file,format=base_track_file[::-1][:base_track_file[::-1].find("."):-1])
+overlay_track = AudioSegment.from_file(path + overlay_track_file,format=overlay_track_file[::-1][:overlay_track_file[::-1].find("."):-1])
 
+if (pos < 0):
+    merge_track = AudioSegment.silent(duration = len(base_track) - pos)
+    merge_track = merge_track.overlay(overlay_track)
+    merge_track = merge_track.overlay(base_track,position=-pos)
+else:
+    merge_track = base_track.overlay(overlay_track,position=pos)
+    
+file_handle = merge_track.export(export_path + "merged_" + base_track_file[::-1][:base_track_file[::-1].find("."):-1] + "_" + overlay_track_file[::-1][:overlay_track_file[::-1].find("."):-1] + ".mp3",format="mp3")
 sys.exit(0)
